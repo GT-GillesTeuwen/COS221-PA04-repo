de63167e6b7742a1bed82c09454ead9ff492691f
GT-GillesTeuwen <gillesteuwen@gmail.com>
2022-04-14 12:37:14 +0200

    first attemp at file commit

---
+++ src/main/java/za/ac/up/cs/cos221/prac04/DBManager.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.up.cs.cos221.prac04;
+
+/**
+ *
+ * @author User
+ */
+public class DBManager {
+
+	public DBManager() { 
+		System.out.println("hi");
+	}
+}
