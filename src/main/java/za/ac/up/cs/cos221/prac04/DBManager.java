6ba5ca59b2f57e9fba3a2e75da6986ee83a22b96
GT-GillesTeuwen <gillesteuwen@gmail.com>
2022-04-14 14:37:52 +0200

    DB connection test

--- src/main/java/za/ac/up/cs/cos221/prac04/DBManager.java
+++ src/main/java/za/ac/up/cs/cos221/prac04/DBManager.java
@@ -5,13 +5,56 @@
  */
 package za.ac.up.cs.cos221.prac04;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author User
  */
-public class DBManager {
+public class DBManager {  
+	
+	
+	
+	public static class implement {
 
-	public DBManager() { 
-		System.out.println("hi");
+		private static Connection con;
+		private static boolean hasData = false;
+		private static String bindAddress ="192.168.8.102:3306";
+
+		
+		public static void setLocal(boolean local){
+			if (local) {
+				bindAddress = "192.168.8.102:3306";
+			}else{
+				bindAddress = "41.157.47.27:3306";
+			}
+		}
+
+		public static boolean testConnection(String username, String password) {
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				con = DriverManager.getConnection("jdbc:mysql://" + bindAddress, username, password);
+				System.out.println("Connected to Server");
+			} catch (ClassNotFoundException ex) {
+
+				Logger.getLogger(DBManager.class.getName()).log(Level.SEVERE, null, ex);
+				con = null;
+				return false;
+			} catch (SQLException ex) {
+				//bindAddress=JOptionPane.showInputDialog("The default database address is unavailable\n Please enter another address");
+				Logger.getLogger(DBManager.class.getName()).log(Level.SEVERE, null, ex);
+				con = null;
+				return false;
+			}
+			con = null;
+			return true;
+		}
+		
+		
+
 	}
 }
