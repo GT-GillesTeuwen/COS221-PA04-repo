794d2a504304af44cb3fc0eee1f9e7c5b08ddaae
GT-GillesTeuwen <gillesteuwen@gmail.com>
2022-04-14 14:35:42 +0200

    DB connection test

--- src/main/java/za/ac/up/cs/cos221/prac04/main.java
+++ src/main/java/za/ac/up/cs/cos221/prac04/main.java
@@ -5,12 +5,16 @@
  */
 package za.ac.up.cs.cos221.prac04;
 
+import za.ac.up.cs.cos221.prac04.GUI.DatabaseSelect;
+
 /**
  *
  * @author User
  */
 public class main {
 	public static void main(String[] args) {
-		DBManager db = new DBManager();
+		DatabaseSelect pane= new DatabaseSelect();
+		pane.setLocationRelativeTo(null);
+		pane.setVisible(true);
 	}
 }
